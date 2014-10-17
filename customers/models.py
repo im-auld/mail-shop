44396cce780b043064b497ed7c7b5722af371aae
Ian Auld <imauld@gmail.com>
2014-10-17 12:46:30 -0700

    Initial. Began work on customer model

---
+++ customers/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.core import validators
+from sorl.thumbnail import ImageField
+
+STATES = (
+    ('AZ', 'Arizona'),
+    ('AK', 'Arkansas'),
+    ('NY', 'New York'),
+    ('WA', 'Washington'),
+)
+
+
+ID_TYPES = (
+    ('DL', 'Drivers License'),
+    ('ND', 'State Non-Driver ID'),
+    ('CC', 'Credit/Debit Card'),
+    ('ST', 'Student ID'),
+    ('OT', 'Other'),
+)
+
+
+class Customer(models.Model):
+    f_name = models.CharField(
+        'first name',
+        max_length=25,
+        help_text='Required. 25 characters or fewer',
+        validators=[
+            validators.MaxLengthValidator(25)
+        ]
+    )
+    l_name = models.CharField(
+        'last name',
+        max_length=25,
+        help_text='Required. 25 characters or fewer',
+        validators=[
+            validators.MaxLengthValidator(25)
+        ]
+    )
+    line_1 = models.CharField('Address Line 1', max_length=50)
+    line_2 = models.CharField('Address Line 2', max_length=50)
+    city = models.CharField(max_length=25)
+    state = models.CharField(choices=STATES)
+    phone_num = models.CharField(
+        'phone number',
+        max_length=15,
+    )
+    first_id = models.CharField(choices=ID_TYPES)
+    second_id = models.CharField(choices=ID_TYPES)
+    first_id_image = ImageField()
