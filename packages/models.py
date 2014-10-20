18ae32eaaed3084d3635752221a395f47b21737c
Ian Auld <imauld@gmail.com>
2014-10-20 12:32:02 -0700

    Added package model

--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    customer = models.ForeignKey('customer.Customer')
+    box_num = models.ForeignKey('mailboxes.Mailbox')
+    date_arrived = models.DateField(auto_now_add=True)
+    date_claimed = models.DateField(blank=True)
+    tracking_number = models.CharField()
+
+    def __init__(self, customer, box_num, tracking_number):
+        self.customer = customer
+        self.box_num = box_num
+        self.tracking_number = tracking_number
