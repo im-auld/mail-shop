e14873ce4c4f16c11f0b51efb37aeccbe306598b
Ian Auld <imauld@gmail.com>
2014-10-21 23:29:12 -0700

    Adjusted mailbox model/admin to work more intuitively

--- mail_shop/mailboxes/admin.py
+++ mail_shop/mailboxes/admin.py
@@ -2,7 +2,51 @@ from django.contrib import admin
 from models import Mailbox
 
 
+class AvailableBoxFilter(admin.SimpleListFilter):
+    title = "Available Boxes"
+    parameter_name = "is_owned"
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', "Available"),
+            ('1', "Unavailable"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '0':
+            return queryset.filter(owner__isnull=True)
+        if self.value() == '1':
+            return queryset.filter(owner__isnull=False)
+
+
 class MailboxAdmin(admin.ModelAdmin):
+    def owner_link(self, obj):
+        if obj.owner:
+            return '<a href="/admin/customers/customer/{}/">{}</a>'.format(
+                obj.owner.id,
+                obj.owner
+            )
+        else:
+            return None
+
+    owner_link.allow_tags = True
+    owner_link.short_description = 'Owner'
     search_fields = ['box_num', 'owner__l_name', 'owner__email']
+    list_display = [
+        'box_num',
+        'size',
+        'next_due_on',
+        'monthly_rate',
+        'owner_link',
+        'is_current',
+    ]
+    list_filter = [
+        'is_current',
+        'used_for_business',
+        'size',
+        'is_current',
+        AvailableBoxFilter
+    ]
+    readonly_fields = ('is_current',)
 
 admin.site.register(Mailbox, MailboxAdmin)
--- mail_shop/mailboxes/models.py
+++ mail_shop/mailboxes/models.py
@@ -16,19 +16,22 @@ RATES = {
 
 
 class Mailbox(models.Model):
-    owner = models.ForeignKey('customers.Customer')
+    owner = models.ForeignKey('customers.Customer', blank=True, null=True)
     box_num = models.IntegerField('Box Number', unique=True, null=False)
     size = models.CharField(max_length=2, choices=BOX_SIZES)
-    owned_since = models.DateField('Owned Since: ', null=True)
-    next_due_on = models.DateField('Next Bill Due: ', null=True)
+    owned_since = models.DateField('Owned Since: ', blank=True, null=True)
+    next_due_on = models.DateField('Next Bill Due: ', blank=True, null=True)
     num_of_users = models.IntegerField('Additional users: ', default=0)
     num_of_key_sets = models.IntegerField('Sets of keys: ', default=1)
     used_for_business = models.BooleanField('Used for business', default=False)
-    is_current = models.BooleanField(default=True)
+    is_current = models.BooleanField('Current', default=True)
+
+    class Meta:
+        verbose_name_plural = 'Mailboxes'
 
     @property
     def monthly_rate(self):
-        return RATES[str(self.size)] + (self.num_of_users * 5)
+        return RATES[str(self.size)] + (self.num_of_users * 5) + (int(self.used_for_business * 5))
 
     @property
     def six_month_rate(self):
@@ -37,6 +40,10 @@ class Mailbox(models.Model):
     @property
     def yearly_rate(self):
         return self.monthly_rate * 12
+
+    @property
+    def is_owned(self):
+        return bool(self.owner)
 
     def __unicode__(self):
         return str(self.box_num)
