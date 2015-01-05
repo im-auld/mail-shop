925650b81fee80e6775b80cf222d320314406346
Ian Auld <imauld@gmail.com>
2015-01-05 13:43:05 -0800

    Added the alerts widget for the dahsboard. There will likely be soe updates to this widget in the future.

--- mail_shop/mail_shop/views.py
+++ mail_shop/mail_shop/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render
 
 from packages.models import Package
@@ -7,7 +8,9 @@ from mailboxes.models import Mailbox, MailboxOwner
 
 def index(request):
     packages = Package.objects.filter(date_claimed__isnull=True).order_by('box_num')
+    alerts = MailboxOwner.objects.filter(due_date__lt=date.today())
     context = {
         'packages': packages,
+        'alerts': alerts,
     }
     return render(request, 'index.html', context)
