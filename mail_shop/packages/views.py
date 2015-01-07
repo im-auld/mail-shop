88820167de6246870537ccc39effd86345a0fd95
Ian Auld <imauld@gmail.com>
2015-01-07 13:38:42 -0800

    Improved pin system with Django forms.

--- mail_shop/packages/views.py
+++ mail_shop/packages/views.py
@@ -47,7 +47,7 @@ def package_form_view(request):
 
 def claim_packages_view(request):
     if request.method == 'POST':
-        package_ids = request.POST.getlist('claimed_packages[]', [])
+        package_ids = request.POST.getlist('unclaimed_packages', [])
         pin = int(request.POST.get('pin', 0))
         packages = validate_claim(package_ids, pin)
         if packages:
@@ -60,7 +60,10 @@ def validate_claim(package_ids, pin):
     packages = Package.objects.filter(id__in=package_ids)
     # TODO: Hash stored pins
     customers = [p.customer for p in packages if p.customer.pin == pin]
-    if customers.count(customers[0]) == len(customers) and customers:
-        return packages
-    else:
+    try:
+        if customers.count(customers[0]) == len(customers) and customers:
+            return packages
+        else:
+            return None
+    except IndexError:
         return None
