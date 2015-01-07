dc065559164c37b51456463f5ee457260398a145
Ian Auld <imauld@gmail.com>
2015-01-06 16:37:42 -0800

    Had to add a fields attribute to my ModelForm. The warning was messing with pdb.

--- mail_shop/customers/forms.py
+++ mail_shop/customers/forms.py
@@ -5,3 +5,15 @@ from customers.models import Customer
 class CustomerForm(ModelForm):
     class Meta:
         model = Customer
+        fields = [
+            'f_name',
+            'l_name',
+            'line_1',
+            'line_2',
+            'city',
+            'state',
+            'zip_code',
+            'phone_num',
+            'email',
+            'photo_file'
+        ]
