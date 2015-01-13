fd14f4f4fa6a9e503ceed9f840283b2cb529eaae
Ian Auld <imauld@gmail.com>
2015-01-13 13:00:44 -0800

    Cleaned up payment view. Fixed some links to the payment view.

--- mail_shop/payments/urls.py
+++ mail_shop/payments/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 payments_patterns = patterns(
     'payments.views',
     url(
-        r'^payments/(?P<mailbox_id>)\d+$',
+        r'^payments/(?P<mailbox_id>\d+)$',
         'payment_view',
         name='filled_payment_view'
     ),
--- mail_shop/payments/views.py
+++ mail_shop/payments/views.py
@@ -4,22 +4,28 @@ from mailboxes.models import MailboxOwner
 from payments.forms import PaymentForm
 
 
-def payment_view(request, mailbox_id):
+def payment_view(request, mailbox_id=None):
     if request.method == 'POST':
         form = PaymentForm(request.POST)
         payment = form.save()
         payment.save()
         return redirect('customers_index')
     else:
-        print(mailbox_id)
-        # import pdb; pdb.set_trace()
-        customer = MailboxOwner.objects.get(box__pk=1)
-        form = PaymentForm(initial={
-            'customer': customer.pk,
-            'amount': customer.monthly_rate,
-            'payment_type': 'Rent'
-        })
+        data = _get_initial_form_data(mailbox_id)
+        form = PaymentForm(initial=data)
         context = {
             'form': form,
         }
         return render(request, 'payments/payment_form.html', context)
+
+def _get_initial_form_data(mailbox_id):
+    try:
+        customer = MailboxOwner.objects.get(box__pk=mailbox_id)
+        data = {
+            'customer': customer.pk,
+            'amount': customer.monthly_rate,
+            'payment_type': 'Rent'
+        }
+        return data
+    except MailboxOwner.DoesNotExist:
+        return {}
